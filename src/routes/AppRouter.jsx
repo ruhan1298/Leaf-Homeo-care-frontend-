aae57dfe85537d6542ab62d18dc20de35230c1b1
ruhan2000 <ruhan.behlim@aminsofttech.co>
2026-07-09 13:13:44 +0530

    first commit

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,4 @@
-// src/routes/AppRouter.jsx
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Register from "../pages/patient/register";
 import Login from "../pages/patient/login";
 import DoctorLogin from "../pages/doctor/login";
@@ -11,22 +10,23 @@ import AdminPatients from "../pages/admin/Patient";
 import AdminAppointments from "../pages/admin/Appointment";
 import AdminProfile from "../pages/admin/Profile";
 import AdminForgotPassword from "../pages/admin/ForgotPassword";
+
 function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/doctor/login" element={<DoctorLogin />} />
       <Route path="/admin/login" element={<AdminLogin />} />
       <Route path="/admin/forgot-password" element={<AdminForgotPassword />} />
-      <Route path ="/admin/dashboard" element={<AdminDashboard />} />
-      <Route path ="/admin/doctors" element={<AdminDoctors />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/doctors" element={<AdminDoctors />} />
       <Route path="/admin/patients" element={<AdminPatients />} />
       <Route path="/admin/appointments" element={<AdminAppointments />} />
       <Route path="/admin/profile" element={<AdminProfile />} />
-
-      <Route path="/patient/dashboard" element={<PatientDashboard />} />      
-</Routes>
+      <Route path="/patient/dashboard" element={<PatientDashboard />} />
+    </Routes>
   );
 }
 
